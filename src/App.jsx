60acd395265fc77ce11e56278cece8017d7c1709
lijun <123>
2024-06-28 13:35:18 +0800

    main page to /cabins

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,17 @@ function App() {
     <QueryClientProvider client={queryClient}>
       {/* react-query */}
       <ReactQueryDevtools initialIsOpen={false} />
-    {/* react-query-del-tool */}
+      {/* react-query-del-tool */}
       <GolbalStyles />
       {/* styled-components-golbal */}
       <BrowserRouter>
-      {/* react-router */}
+        {/* react-router */}
         <Routes>
           <Route element={<AppLayout />}>
-            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/" element={<Navigate replace to="cabins" />} />
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="bookings" element={<Bookings />} />
-            <Route path="cabins" element={<Cabins />} />
+            <Route index path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
--- src/ui/Ps.jsx
+++ src/ui/Ps.jsx
@@ -12,13 +12,7 @@ function Ps() {
     <div>
       <P>仍在开发中</P>
       <P>Ps:</P>
-      <P>
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        本网站使用React搭建的单页式应用，使用react-router实现前端路由，使用context
-        APi实现可读性优化，使用了高阶复合组件
-      </P>
-      <P>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;后端由suPabase快速搭建</P>
-      <P>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;网站交由netlify托管</P>
+      <P>目前只写了客房，设置页</P>
     </div>
   );
 }
